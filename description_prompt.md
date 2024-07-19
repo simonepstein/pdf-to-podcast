5c59ccfebc060aeda37e35f7a5f8c6e80b2e3d5d
Simon Epstein <simon.epstein@67bricks.com>
2024-07-19 16:05:08 +0100

    Added structured url support.

---
+++ description_prompt.md
@@ -0,0 +1,5 @@
+<p style="text-align:left">
+  <strong>Upload a PDF or HTML file to convert it into a podcast</strong>
+  <br>
+  <a href="https://github.com/knowsuchagency/pdf-to-podcast">knowsuchagency/pdf-to-podcast</a>
+</p>
--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class DialogueOptions(BaseModel):
             return ""
     def organisation_prompt(self) -> str:
         if self.organisation.strip():
-            return "The podcast is being produced by " + self.organisation + ". Be sure to mention this in the podcast/"
+            return "The podcast is being produced by " + self.organisation + ". Be sure to mention this in the podcast."
         else:
             return ""
 
@@ -149,6 +149,11 @@ def generate_dialogue(text: str, title_prompt: str, audience_prompt1: str, audie
     At the end of the dialogue, have the host and guest speakers naturally summarize the main insights and takeaways from their discussion. This should flow organically from the conversation, reiterating the key points in a casual, conversational manner. Avoid making it sound like an obvious recap - the goal is to reinforce the central ideas one last time before signing off.
     </podcast_dialogue>
     """
+
+@retry(retry=retry_if_exception_type(ValidationError))
+@llm(model="gpt-4o", max_tokens=4096)
+def generate_dialogue_custom_prompt(custom_prompt: str) -> Dialogue:
+    """{custom_prompt}"""
 
 def get_mp3(text: str, voice: str, api_key: str = None) -> bytes:
     client = OpenAI(
@@ -228,6 +233,17 @@ def generate_audio(text: str, options: DialogueOptions, openai_api_key: str = No
     )
     return generate_audio_from_dialogue(llm_output, openai_api_key)
 
+def generate_audio_custom_prompt(custom_prompt: str, openai_api_key: str = None) -> bytes:
+    logger.info("===== Generating audio custom prompt =====")
+    logger.info(f"Custom prompt: {custom_prompt}")
+
+    if len(custom_prompt.strip()) == 0:
+        raise gr.Error("Prompt is empty")
+
+    llm_output = generate_dialogue_custom_prompt(custom_prompt)
+
+    return generate_audio_from_dialogue(llm_output, openai_api_key)
+
 def handle_file_upload(file:str, title:str, organisation:str, audience:str, participant1:str, participant2: str, language: str, with_metadata: bool, openai_api_key: str = None) -> bytes:
     if not os.getenv("OPENAI_API_KEY", openai_api_key):
         raise gr.Error("OpenAI API key is required")
@@ -267,6 +283,44 @@ def handle_transcript(transcript:str, openai_api_key: str = None) -> bytes:
     dialog_items = [DialogueItem(text=l.split(':', 1)[1], speaker=l.split(':', 1)[0]) for l in transcript.split("\n\n") if l.strip()]
     dialog = Dialogue(scratchpad="", dialogue=dialog_items)
     return generate_audio_from_dialogue(dialog, openai_api_key)[0]
+
+def handle_prompt(file:str, preamble:str, branding:str, approach:str, scratchpad:str, podcast_dialogue:str, openai_api_key: str = None) -> bytes:
+    if not os.getenv("OPENAI_API_KEY", openai_api_key):
+        raise gr.Error("OpenAI API key is required")
+    logger.info("===== Processing file with custom prompt =====")
+    logger.info(file)
+    with Path(file).open("rb") as f:
+        if file.endswith(".pdf"):
+            logger.info("===== Processing PDF =====")
+            reader = PdfReader(f)
+            text = "\n\n".join([page.extract_text() for page in reader.pages])
+        else:
+            soup = BeautifulSoup(f, 'html.parser')
+            text = soup.get_text(separator=' ', strip=True)
+
+        custom_prompt = f"""
+{preamble}
+
+Here is the input text you will be working with:
+
+<input_text>
+{text}
+</input_text>
+
+{branding}
+
+{approach}
+
+<scratchpad>
+{scratchpad}
+</scratchpad>
+
+<podcast_dialogue>
+{podcast_dialogue}
+</podcast_dialogue>
+
+"""
+        return generate_audio_custom_prompt(custom_prompt, openai_api_key)
 
 def common_components():
     """Need to instantiate these separately because they are used in multiple places.
@@ -363,11 +417,90 @@ transcript_interface = gr.Interface(
     api_name=False,
 )
 
+
+prompt_interface = gr.Interface(
+    description=Path("description_prompt.md").read_text(),
+    fn=handle_prompt,
+    inputs = [
+        gr.File(
+            label="File (html or pdf)",
+        ),
+        gr.Textbox(
+            label="Preamble",
+            lines=3,
+            value=
+"""Your task is to take the input text provided and turn it into an engaging, informative podcast dialogue in English. The input text may be messy or unstructured, as it could come from a variety of sources like PDFs or web pages. Don't worry about the formatting issues or any irrelevant information; your goal is to extract the key points and interesting facts that could be discussed in a podcast.
+"""
+        ),
+        gr.Textbox(
+            label="Branding",
+            lines=5,
+            value=
+"""The podcast should be called "The rest is data"
+
+The podcast is being produced by 67 Bricks. Be sure to mention this in the podcast.
+
+The participants in the podcast are Simon (evangelist) and Alex (researcher).  use their roles to frame how they contribute to the discussion.
+
+First, carefully read through the input text and identify the main topics, key points, and any interesting facts or anecdotes. Think about how you could present this information in a fun, engaging way that would be suitable for an audio podcast.
+"""
+        ),
+        gr.Textbox(
+            label="Approach",
+            lines=3,
+            value=
+"""First, carefully read through the input text and identify the main topics, key points, and any interesting facts or anecdotes. Think about how you could present this information in a fun, engaging way that would be suitable for an audio podcast.
+"""
+        ),
+        gr.Textbox(
+            label="Scratchpad",
+            lines=5,
+            value=
+"""Brainstorm creative ways to discuss the main topics and key points you identified in the input text. For a general audience, consider using analogies, storytelling techniques, or hypothetical scenarios to make the content more relatable and engaging for listeners.
+
+Keep in mind that your podcast should be accessible to a general audience, so avoid using too much jargon or assuming prior knowledge of the topic. If necessary, think of ways to briefly explain any complex concepts in simple terms.
+
+Use your imagination to fill in any gaps in the input text or to come up with thought-provoking questions that could be explored in the podcast. The goal is to create an informative and entertaining dialogue, so feel free to be creative in your approach.
+
+Write your brainstorming ideas and a rough outline for the podcast dialogue here. Be sure to note the key insights and takeaways you want to reiterate at the end.
+"""
+        ),
+        gr.Textbox(
+            label="Dialogue",
+            lines=5,
+            value=
+"""Write your engaging, informative podcast dialogue here, based on the key points and creative ideas you came up with during the brainstorming session. 
+
+Use a conversational tone and include any necessary context or explanations to make the content accessible to a general audience.
+
+Use made-up names for the hosts and guests to create a more engaging and immersive experience for listeners. Do not include any bracketed placeholders like [Host] or [Guest]. Design your output to be read aloud -- it will be directly converted into audio.
+
+Start by introducing the subject of the podcast, citing the title and any main authors you have identified.
+You can talk about the authors but they do not participate in the podcast.
+
+Make the dialogue as long and detailed as possible, while still staying on topic and maintaining an engaging flow. Aim to use your full output capacity to create the longest podcast episode you can, while still communicating the key information from the input text in an entertaining way.
+
+At the end of the dialogue, have the host and guest speakers naturally summarize the main insights and takeaways from their discussion. This should flow organically from the conversation, reiterating the key points in a casual, conversational manner. Avoid making it sound like an obvious recap - the goal is to reinforce the central ideas one last time before signing off.
+"""
+        ),
+    ],
+    outputs=[
+        gr.Audio(label="Audio", format="mp3"),
+        gr.Textbox(label="Transcript", show_copy_button=True),
+    ],
+    allow_flagging="never",
+    clear_btn=None,
+    cache_examples="lazy",
+    api_name=False,
+)
+
+
+
 demo = gr.TabbedInterface(
     title="PDF or Web page to Podcast",
     head=os.getenv("HEAD", "") + Path("head.html").read_text(), 
-    interface_list=[file_interface, url_interface, transcript_interface],
-    tab_names=["Convert file", "Convert web page", "Process transcript (advanced)"],
+    interface_list=[file_interface, url_interface, transcript_interface, prompt_interface],
+    tab_names=["Convert file", "Convert web page", "Process transcript (advanced)", "Edit prompt (advanced)"],
 )
 demo = demo.queue(
     max_size=20,

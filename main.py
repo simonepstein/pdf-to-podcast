e96369368c6a5940aa78f946f51f646e28e1eb3a
Simon Epstein <simon.epstein@67bricks.com>
2024-09-24 12:01:45 +0100

    Set a user agent to workaround simple bot blocking.

--- main.py
+++ main.py
@@ -289,7 +289,11 @@ def handle_url(url:str, title:str, organisation:str, audience:str, participant1:
     logger.info("===== Fetching URL =====")
     logger.info(url)
     try:
-        response = requests.get(url)
+        # Set a custom User-Agent to mimic a browser request, OUP for example blocks simple bots
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             text = soup.get_text(separator=' ', strip=True)
